5f5801b91fe4c78cf33027d85e0114837a9d7d50
xiaobeii <guliwei@live.cn>
2014-09-14 19:46:39 +0800

    添加活动事件的接口测试
    添加活动接口活动测试

--- application/controllers/event.php
+++ application/controllers/event.php
@@ -17,13 +17,45 @@ class Event extends CI_Controller {
      * map to /index.php/welcome/<method_name>
      * @see http://codeigniter.com/user_guide/general/urls.html
      */
+
+    public function __construct() {
+        parent::__construct ();
+        $this-> load->model('event_model');
+        //$this->load->helper(array('form', 'url'));
+    }
+
+    //初次载入
+    //参数还没有定义
     public function index()
     {
+        $res = $this->event_model -> event_query(1,0,20);
+        $data['res'] = $res;
+
+
         $this->load->view('mianHeader');
-        $this->load->view('eventBodyView');
+        $this->load->view('eventBodyView', $data);
         $this->load->view('mainFooter');
     }
-    
+
+    //根据需要分页获取其他的活动，AJAX调用
+    public function get_other_event($currentpage,$tag,$pagesize)
+    {
+        //错误预警判断
+
+        //调用DB
+        $res = $this->event_model -> event_query($currentpage, $tag, $pagesize);
+        $data['res'] = $res;
+
+        return $data;
+    }
+
+    //获取活动页Banner大图
+    public  function get_banner_picture()
+    {
+
+    }
+
+
 }
 
 /* End of file welcome.php */
